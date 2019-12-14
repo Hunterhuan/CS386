5a6d81f3a8edeaaa4d298ea163a9d9d52af16778
Lausichen <dyielsc@sjtu.edu.cn>
2019-12-14 19:18:43 +0800

    changepre

--- SVM/datapreprocess.py
+++ SVM/datapreprocess.py
@@ -5,29 +5,29 @@ import numpy as np
 
 
 def load_data():
-    with open("../train.json", "r")as f:
+    with open("train.json", "r")as f:
         train_set = json.load(f)
-    with open("../val.json", "r")as f:
+    with open("val.json", "r")as f:
         val_set = json.load(f)
-    if not os.path.exists("traindataset"):
-        os.makedirs("traindataset")
+    if not os.path.exists("SVM/traindataset"):
+        os.makedirs("SVM/traindataset")
     for key in train_set.keys():
         for pic in train_set[key]:
-            picpath = "../dataset/{0}/{1}".format(key, pic)
+            picpath = "dataset_binary/{0}/{1}".format(key, pic)
             img = Image.open(picpath, "r")
             img = data_preprocess(img)
-            with open("traindataset/traindata.txt", "a+") as f:
+            with open("SVM/traindataset/traindata.txt", "a+") as f:
                 for i in img:
                     f.write("{0}\t".format(i))
                 f.write("{0}\n".format(int(key)))
-    if not os.path.exists("valdataset"):
-        os.makedirs("valdataset")
+    if not os.path.exists("SVM/valdataset"):
+        os.makedirs("SVM/valdataset")
     for key in val_set.keys():
         for pic in val_set[key]:
-            picpath = "../dataset/{0}/{1}".format(key, pic)
+            picpath = "dataset_binary/{0}/{1}".format(key, pic)
             img = Image.open(picpath, "r")
             img = data_preprocess(img)
-            with open("valdataset/valdata.txt", "a+") as f:
+            with open("SVM/valdataset/valdata.txt", "a+") as f:
                 for i in img:
                     f.write("{0}\t".format(i))
                 f.write("{0}\n".format(int(key)))
